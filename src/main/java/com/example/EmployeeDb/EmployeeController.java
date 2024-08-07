18c573b2e226dfec1d6e12052f3bfde28326ac2d
Chrisin25 <chrising2001@gmail.com>
2024-08-07 15:33:08 +0530

    second commit

--- src/main/java/com/example/EmployeeDb/EmployeeController.java
+++ src/main/java/com/example/EmployeeDb/EmployeeController.java
@@ -28,7 +28,6 @@ import com.example.EmployeeDb.models.EmployeeResponse;
 import com.example.EmployeeDb.models.EmployeeResponseGet;
 import com.example.EmployeeDb.models.Input;
 import com.example.EmployeeDb.repository.EmployeeRepository;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -38,40 +37,41 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RequestMapping("/")
 @EnableMongoRepositories
 public class EmployeeController {
-
+ 
     @Autowired
     EmployeeRepository employeeRepo;
     @PostMapping("/AddEmployee")
     ResponseEntity <Map<String,String>> addEmployeesService(@Valid @RequestBody Employee employee){
-
         Map<String,String> result=new HashMap<>();
         
         //calculate year of experience
         employee.setYearOfExperience(Period.between(employee.getDateOfJoining().toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), LocalDate.now()).getYears());
-        //check designation (account manager/associate)
-        if(!(employee.getDesignation().matches("Account Manager") || employee.getDesignation().matches("Associate"))){
-            result.put("message ","Invalid Designation");
-            return new ResponseEntity<>(result,HttpStatus.BAD_REQUEST);
-        }
-        //email validation
-        //check department(sales/delivery/qa/engineering/ba)
-        if(!(employee.getDepartment().matches("sales") || employee.getDepartment().matches("delivery") || employee.getDepartment().matches("QA") || employee.getDepartment().matches("engineering") || employee.getDepartment().matches("ba"))){
-            result.put("message ","Invalid Department");
-            return new ResponseEntity<>(result,HttpStatus.BAD_REQUEST);
-        }
-        //mobile validation
+        
         //manager id validation
-         if(Integer.parseInt(employee.getId())<=0 && employee.getDesignation()!="Account Manager"){
+        if(Integer.parseInt(employee.getId())<=0 && employee.getDesignation()!="Account Manager"){
             result.put("message ","Invalid Id");
+            return new ResponseEntity<>(result,HttpStatus.BAD_REQUEST);
+        }
+
+        if(employeeRepo.existsById(employee.getId())){
+            result.put("message ","Employee id already exist");
             return new ResponseEntity<>(result,HttpStatus.BAD_REQUEST);
         }
         
         //add to db
         Date today=new Date();
         employee.setCreatedTime(today);
-        employeeRepo.save(employee);
+        if((employeeRepo.existsById(employee.getManagerId())==true && employee.getDesignation()=="Associate")||
+        employeeRepo.existsById(employee.getManagerId())==false && employee.getDesignation()=="Account Manager"){
+            employeeRepo.save(employee);
+            result.put("message ","successfully created");
+        }
+        else{
+            result.put("message ","Employee cannot be created in this department");
+            return new ResponseEntity<>(result,HttpStatus.BAD_REQUEST);
+        }
         //print success message
-        result.put("message ","successfully created");
+        
         return ResponseEntity.ok(result);//new ResponseEntity<>(result,HttpStatus.OK);
     }
 
@@ -83,29 +83,34 @@ public class EmployeeController {
         try{
             //find employee using id
             Employee e= employeeRepo.findEmployee(input.getEmployeeId());
-            //System.out.println(input.getEmployeeId());
-            //System.out.println(e);
+            
+           try{ 
             String old=e.getManagerId();
-            //System.out.println(old);
             //get previous manager 
             Employee m1=employeeRepo.findEmployee(old);
-            //System.out.println(m1);
+            Employee m2=employeeRepo.findEmployee(input.getManagerId());
             e.setManagerId(input.getManagerId());
+            e.setDepartment(m2.getDepartment());
             Date today=new Date();
             e.setUpdatedTime(today);
             employeeRepo.save(e);
             result.put("message ",""+ e.getName()+"'s manager has been successfully changed from "+m1.getName()+" to "+employeeRepo.findEmployee(input.getManagerId()).getName()+".");
-            
+           }
+           
+           catch(NullPointerException n){
+            System.out.println(n);
+            result.put("message ","Manager not found");
+            return new ResponseEntity<>(result,HttpStatus.BAD_REQUEST);
+        }
 
         }
         catch(IllegalArgumentException i){
-            //System.out.println(i+" specify id");
-            result.put("message ","enter id");
+            result.put("message ","Enter valid id");
             return new ResponseEntity<>(result,HttpStatus.BAD_REQUEST);
         }
         catch(NullPointerException n){
             System.out.println(n);
-            result.put("message ","manager not found");
+            result.put("message ","Employee not found");
             return new ResponseEntity<>(result,HttpStatus.BAD_REQUEST);
         }
         
@@ -120,24 +125,18 @@ public class EmployeeController {
  
     // Retrieve all employees
     List<Employee> employees = employeeRepo.findAll();
-    //System.out.println(employees);
     // Group employees by manager ID
     Map<String, List<Employee>> employeesByManager = employees.stream()
         .collect(Collectors.groupingBy(Employee::getManagerId));
-    //System.out.println(employeesByManager);
     // Process the employees to filter and map to the response type
     List<EmployeeResponse> filteredResponses = employeesByManager.entrySet().stream()
         .map(entry -> {
             String currentManagerId = entry.getKey();
-            //System.out.println("manager id"+currentManagerId);
             List<Employee> employeeList = entry.getValue();
-            //System.out.println(employeeList);
             // get manager details
             Optional<Employee> managerOpt = employeeRepo.findById(currentManagerId);
-            //System.out.println("manager"+managerOpt);
             String managerNameOpt = managerOpt.map(Employee::getName).orElse("Unknown");
             String managerDept = managerOpt.map(Employee::getDepartment).orElse("Unknown");
-            //System.out.println("manager details: "+managerNameOpt+managerDept);
             // Filter employees based on provided criteria
             List<Employee> filteredEmployeeList = employeeList.stream()
                 .filter(employee -> {
@@ -155,7 +154,6 @@ public class EmployeeController {
                     return false;
                 })
                 .collect(Collectors.toList());
-                //System.out.println("filtered"+filteredEmployeeList);
  
             if (!filteredEmployeeList.isEmpty() && Integer.parseInt(currentManagerId) > 0) {
                 return new EmployeeResponse(
@@ -170,22 +168,16 @@ public class EmployeeController {
         })
         .filter(Objects::nonNull) // Filter out null values
         .collect(Collectors.toList());
-        //System.out.println(filteredResponses);
-    // Create the response message
     String responseMessage = filteredResponses.isEmpty() ? "No employees found" : "Employees retrieved successfully";
  
-    // Wrap the result in EmployeeResponseGet
     EmployeeResponseGet response = new EmployeeResponseGet(responseMessage, filteredResponses);
-    System.out.println(response);
-    // Return the wrapped result with ResponseEntity
     return ResponseEntity.ok(response);
 }
     @DeleteMapping("/remove")
     ResponseEntity <Map<String,String>> DeleteEmployeeService(@RequestParam String id) {
-        System.out.println(id);
         Map<String,String> result=new HashMap<>();
+        try{
         Employee e= employeeRepo.findEmployee(id);
-        System.out.println(e);
         if(e.getDesignation().matches("Account Manager")){
             //check whether employees exists under manager
             if(employeeRepo.employeesUnderManager(id)==null){
@@ -202,6 +194,12 @@ public class EmployeeController {
             
         }
         return new ResponseEntity<>(result,HttpStatus.OK);
+    }
+    catch(NullPointerException n){
+        result.put("message","Employee doesnot exist");
+        return new ResponseEntity<>(result,HttpStatus.BAD_REQUEST);
+        
+    }
     }
     
 }
--- src/main/java/com/example/EmployeeDb/models/Employee.java
+++ src/main/java/com/example/EmployeeDb/models/Employee.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import org.springframework.data.annotation.Id;
@@ -17,12 +18,14 @@ public class Employee {
     private String name;
     @Id
     private String id;
+    @Pattern(regexp = "Account Manager|Associate",message = "invalid designation")
     private String designation;
+    @Pattern(regexp="sales|delivery|QA|engineering|BA",message = "invalid department")
     private String department;
-    @Email(message = "Email is not valid", regexp="{(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])}")
+    @Email(message = "Email is not valid")
     private String email;
     @NotBlank(message = "mobileNumber is required")
-    @Size(min = 10, max = 10)
+    @Size(min = 10, max = 10,message = "mobile number should contain exactly 10 digits")
     private String mobile;
     private String location;
     private Date dateOfJoining;
